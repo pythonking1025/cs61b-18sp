879e8baa72c9f78f071f2777363c07918d91b610
Jacky Zhuo <473729022@qq.com>
2021-03-09 17:18:21 +0800

    proj2

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,44 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private static final int WIDTH = 50;
+    private static final int HEIGHT = 50;
+
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    public static void addHexagon(TETile[][] world, int size, int x, int y) {
+        int width = 2 * (size - 1) + size;
+        for (int i = 1; i <= size; i ++ ) {
+            for (int j = 1; j <= width; j ++ ) {
+                if ((j > size - i) && (j <= width - size + i)){
+                    world[y + j - 1][x + i - 1] = Tileset.FLOWER;
+                }
+            }
+        }
+        x = x + size;
+        for (int i = 1; i <= size; i ++ ) {
+            for (int j = 1; j <= width; j ++ ) {
+                if ((j > i - 1) && (j <= width - i + 1)){
+                    world[y + j - 1][x + i - 1] = Tileset.FLOWER;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] randomTiles = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                randomTiles[x][y] = Tileset.MOUNTAIN;
+            }
+        }
+        addHexagon(randomTiles, 5, 12, 12);
+
+        ter.renderFrame(randomTiles);
+    }
 }
