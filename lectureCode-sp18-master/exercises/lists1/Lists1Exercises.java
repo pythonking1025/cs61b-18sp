f80ab2c8fcb63676903c2e69c53d873e5bdcdf9f
Jacky Zhuo <473729022@qq.com>
2021-05-18 15:46:19 +0800

    some changes

---
+++ lectureCode-sp18-master/exercises/lists1/Lists1Exercises.java
@@ -0,0 +1,48 @@
+public class Lists1Exercises {
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. L is not allowed
+      * to change. */
+    public static IntList incrList(IntList L, int x) {
+        IntList Q = new IntList(0, null);
+        IntList p = Q;
+        int i = 0;
+        while (i < L.size() - 1)
+            {
+                p.first = L.get(i) + x;
+                p.rest = new IntList(0, null);
+                p = p.rest;
+                i += 1;
+            }
+        p.first = L.get(i);
+        return Q;        
+    }
+
+    /** Returns an IntList identical to L, but with
+      * each element incremented by x. Not allowed to use
+      * the 'new' keyword. */
+    public static IntList dincrList(IntList L, int x) {
+        IntList p = L;
+        while(p.rest != null)
+        {
+            p.first += x;
+            p = p.rest;
+        }
+        return L;
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(5, null);
+        L.rest = new IntList(7, null);
+        L.rest.rest = new IntList(9, null);
+
+        System.out.println(L.size());
+        System.out.println(L.iterativeSize());
+
+        // Test your answers by uncommenting. Or copy and paste the
+        // code for incrList and dincrList into IntList.java and
+        // run it in the visualizer.
+        System.out.println(L.get(1));
+        System.out.println(incrList(L, 3).size());
+        System.out.println(dincrList(L, 3).size());        
+    }
+}
